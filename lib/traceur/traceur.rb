7706f93f40248c5f500f304439ba4d022d0fda26
Mike Williamson <blessedbyvirtuosity@gmail.com>
2013-03-21 16:41:32 -0400

    Labels are now displayed for each method call
    Additionally the document is now sized to the drawing.

--- lib/traceur/traceur.rb
+++ lib/traceur/traceur.rb
@@ -10,12 +10,62 @@ class Traceur
   @svg_file = nil
   @trace_points = []
   @x = 0
-  @y = 0
+  @y = 100
+  @max_y = @y
   @footer = <<-EOSVG
   </g>
 </svg>
 EOSVG
-  @header = <<-EOXML
+
+  def self.watch_paths(expr, output_dir)
+    raise NoBlockError "You must call this method with a block" unless block_given?
+    block = Proc.new #this grabs the block
+
+    raise SystemCallError "Directory does not exist" unless Dir.exists?(output_dir)
+
+    labels = []
+    points = []
+    expression = Regexp.new expr
+    proc = Proc.new do |event, file, line, methodname, binding, classname|
+      if expression.match file
+        @start_time ||= Time.now.strftime("%s.%L").to_f
+        @x += 1
+        if event.to_s == "call"
+          points << " #{@x},#{@y}"
+          labels << ["#{classname}##{methodname}", [@x + 1, @y]]
+          @y += 10
+          @max_y = @y if @y > @max_y
+          points << " #{@x},#{@y}"
+        elsif event.to_s == "return"
+          points << " #{@x},#{@y}"
+          @y -= 10
+          points << " #{@x},#{@y}"
+          labels << ["#{classname}##{methodname}", [@x - 1, @y]]
+        end
+      end
+    end
+    Object.send(:set_trace_func, proc)
+    block.call
+    Object.send(:set_trace_func, nil)
+    write_svg points, labels, output_dir, {x: @x, y: @max_y}
+  end
+
+  private
+
+  def self.write_svg points, labels, directory, dimensions
+    filename = "trace_#{Time.now.strftime('%Y%m%d%H%M%S')}.svg"
+    @svg_file = File.open(File.join(directory, filename), 'w+')
+    @svg_file.write(header(dimensions))
+    @svg_file.write(polygon(points))
+    labels.each do |details|
+      @svg_file.write label(details)
+    end
+    @svg_file.write(@footer)
+    @svg_file.close
+  end
+
+  def self.header dimensions
+  header = <<-EOXML
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <!-- Created with Inkscape (http://www.inkscape.org/) -->
 
@@ -30,7 +80,9 @@ EOSVG
    id="svg2"
    version="1.1"
    inkscape:version="0.48.2 r9819"
-   sodipodi:docname="drawing.svg">
+   sodipodi:docname="drawing.svg"
+   width="#{dimensions[:x]}"
+   height="#{dimensions[:y]}">
   <defs
      id="defs4" />
   <sodipodi:namedview
@@ -40,17 +92,15 @@ EOSVG
      borderopacity="1.0"
      inkscape:pageopacity="0.0"
      inkscape:pageshadow="2"
-     inkscape:zoom="0.35"
-     inkscape:cx="375"
-     inkscape:cy="520"
+     inkscape:zoom="1"
      inkscape:document-units="px"
      inkscape:current-layer="layer1"
      showgrid="false"
-     inkscape:window-width="1440"
-     inkscape:window-height="876"
-     inkscape:window-x="0"
-     inkscape:window-y="24"
-     inkscape:window-maximized="1" />
+     inkscape:window-maximized="1"
+     fit-margin-top="0"
+     fit-margin-left="0"
+     fit-margin-right="0"
+     fit-margin-bottom="0" />
   <metadata
      id="metadata7">
     <rdf:RDF>
@@ -67,50 +117,20 @@ EOSVG
      inkscape:groupmode="layer"
      id="layer1">
 EOXML
-
-  def self.watch_paths(expr, output_dir)
-    raise NoBlockError "You must call this method with a block" unless block_given?
-    block = Proc.new #this grabs the block
-
-    raise SystemCallError "Directory does not exist" unless Dir.exists?(output_dir)
-
-    labels = []
-    points = []
-    expression = Regexp.new expr
-    proc = Proc.new do |event, file, line, methodname, binding, classname|
-      if expression.match file
-        @start_time ||= Time.now.strftime("%s.%L").to_f
-        @x += 1 unless ((@start_time - Time.now.strftime("%s.%L").to_f).abs <= 0.1)
-        if event.to_s == "call"
-          points << " #{@x},#{@y}"
-          labels << [methodname, [@x + 5, @y - 3]]
-          @y += 10
-          points << " #{@x},#{@y}"
-        elsif event.to_s == "return"
-          points << " #{@x},#{@y}"
-          @y -= 10
-          points << " #{@x},#{@y}"
-        end
-      end
-    end
-    Object.send(:set_trace_func, proc)
-    block.call
-    Object.send(:set_trace_func, nil)
-    write_svg points, labels, output_dir
   end
 
-  private
-
-  def self.write_svg points, labels, directory
-    filename = "trace_#{Time.now.strftime('%Y%m%d%H%M%S')}.svg"
-    @svg_file = File.open(File.join(directory, filename), 'w+')
-    @svg_file.write(@header)
-    @svg_file.write(polygon(points))
-    @svg_file.write(@footer)
-    @svg_file.close
-  end
   def self.polygon points
-    "<polyline points=\"0,0#{ points.join }\" style=\"fill:none;stroke:black;stroke-width:3\" />"
+    "<polyline points=\"0,100#{ points.join }\" style=\"fill:none;stroke:black;stroke-width:1;opacity:0.5\" />"
+  end
+
+  def self.label details
+    text =<<-EOTXT
+       <text style="font-size:1px;fill:#000000;stroke:none;font-family:Sans"
+       transform="rotate(90 #{details[1][0]},#{details[1][1]})"
+       x="#{details[1][0]}"
+       y="#{details[1][1]}"
+       id="text2988">#{details[0]}</text>
+    EOTXT
   end
 
 end
